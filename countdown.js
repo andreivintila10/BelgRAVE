badedb27621b27e25cf97ee3498f009ed4df1a2b
Andrei Vintila <andrei.vintila@student.manchester.ac.uk>
2017-05-14 00:50:32 +0100

    Added music, changed the counter layout

--- countdown.js
+++ countdown.js
@@ -1,10 +1,24 @@
 // Set the date we're counting down to.
 var countDownDate = new Date("June 9, 2017 20:00:00").getTime();
-var demo;
+var demo, daysD, hoursD, minutesD, secondsD;
+
 
 do {
-  demo = document.getElementById("demo");
-} while (demo == null);
+  daysD = document.getElementById("days");
+} while (daysD == null);
+
+do {
+  hoursD = document.getElementById("hours");
+} while (hoursD == null);
+
+do {
+  minutesD = document.getElementById("minutes");
+} while (minutesD == null);
+
+do {
+  secondsD = document.getElementById("seconds");
+} while (secondsD == null);
+
 
 // Update the count down every 1 second.
 var x = setInterval(function() {
@@ -19,9 +33,13 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+  
   // Display the result in the element with id="demo".
-  demo.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  // demo.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  daysD.innerHTML = days + "<p style=\"font-size:20px;\">d</p>";
+  hoursD.innerHTML = hours + "<p style=\"font-size:20px;\">h</p>";
+  minutesD.innerHTML = minutes + "<p style=\"font-size:20px;\">m</p>";
+  secondsD.innerHTML = seconds + "<p style=\"font-size:20px;\">s</p>";
 
   // If the count down is finished, write some text.
   if (distance < 0) {
