3781fb366f48cf8fd7ee89012502c3480c32ee3f
Andrei Vintila <andrei.vintila@student.manchester.ac.uk>
2017-05-14 18:50:09 +0100

    Added message for when the counter finishes. A favicon and made the background image a bit darker and blurry.

--- countdown.js
+++ countdown.js
@@ -1,49 +1,50 @@
 // Set the date we're counting down to.
 var countDownDate = new Date("June 9, 2017 20:00:00").getTime();
 var demo, daysD, hoursD, minutesD, secondsD;
+var now, distance, days, hours, minutes, seconds;
 
 
 do {
-  daysD = document.getElementById("days");
+  daysD = document.getElementById("d_d");
 } while (daysD == null);
 
 do {
-  hoursD = document.getElementById("hours");
+  hoursD = document.getElementById("d_h");
 } while (hoursD == null);
 
 do {
-  minutesD = document.getElementById("minutes");
+  minutesD = document.getElementById("d_m");
 } while (minutesD == null);
 
 do {
-  secondsD = document.getElementById("seconds");
+  secondsD = document.getElementById("d_s");
 } while (secondsD == null);
 
 
 // Update the count down every 1 second.
 var x = setInterval(function() {
+
   // Get todays date and time.
-  var now = new Date().getTime();
+  now = new Date().getTime();
 
   // Find the distance between now an the count down date.
-  var distance = countDownDate - now;
+  distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds.
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-  // Display the result in the element with id="demo".
-  // demo.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-  daysD.innerHTML = days + "<p style=\"font-size:20px;\">d</p>";
-  hoursD.innerHTML = hours + "<p style=\"font-size:20px;\">h</p>";
-  minutesD.innerHTML = minutes + "<p style=\"font-size:20px;\">m</p>";
-  secondsD.innerHTML = seconds + "<p style=\"font-size:20px;\">s</p>";
+  days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // If the count down is finished, write some text.
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+  if (distance > 0) {
+    daysD.innerHTML = days;
+    hoursD.innerHTML = hours;
+    minutesD.innerHTML = minutes;
+    secondsD.innerHTML = seconds;
   }
+  else {
+    clearInterval(x);
+    document.getElementById("title").innerHTML = "IT'S TIME TO PARTY!!!";
+  }
+
 }, 1000);
